018d7ab8141ac01513d6b216cc5387d7ec6cdafc
Cosmin TIRU <ctiru@isoftwaresystems.net>
2016-10-04 19:06:11 +0300

    added initial project

---
+++ PSSC/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Class1
+    {
+        //remove or alter this class, then implement your models.  
+    }
+}
